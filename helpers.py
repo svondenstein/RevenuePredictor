d1c8443d8975515a9b66ce945c7530a1b24eeea7
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-08 01:45:23 +0000

    Add automatic saving and reloading of model parameters. Closes #6

--- helpers.py
+++ helpers.py
@@ -1,3 +1,7 @@
+#
+# Stephen Vondenstein
+# 10/07/2018
+#
 import os
 
 def get_data_paths_list(image_folder, mask_folder):
@@ -9,3 +13,23 @@ def get_data_paths_list(image_folder, mask_folder):
                   for x in image_paths]
 
     return image_paths, mask_paths
+
+def save_model_params(growth_k, layers_per_block, num_classes, ckpt_path):
+    param_path = ckpt_path.rsplit('/', 1)[0]
+    param_path = param_path + '/model.params'
+    print("Model parameters saved to: " + param_path)
+    file = open(param_path, "w")
+    data = [str(growth_k) + "\n", layers_per_block + "\n", str(num_classes) + "\n"]
+    file.writelines(data)
+    file.close()
+
+def load_model_params(ckpt_path):
+    param_path = ckpt_path.rsplit('/', 1)[0]
+    param_path = param_path + '/model.params'
+    print("Reading model paramaters from: " + param_path)
+    file = open(param_path, "r")
+    growth_k = int(file.readline())
+    layers_per_block = file.readline()
+    num_classes = int(file.readline())
+    file.close()
+    return growth_k, layers_per_block, num_classes
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 #
 import argparse
 from helpers import get_data_paths_list
+from helpers import save_model_params
+from helpers import load_model_params
 from model import DenseTiramisu
 
 parser = argparse.ArgumentParser()
@@ -34,14 +36,17 @@ parser.add_argument("--prior_model", default="")
 
 def main():
     FLAGS = parser.parse_args()
-    layers_per_block = [int(x) for x in FLAGS.layers_per_block.split(",")]
-
-    tiramisu = DenseTiramisu(FLAGS.growth_k, layers_per_block, FLAGS.num_classes)
 
     if FLAGS.mode == 'train':
+        save_model_params(FLAGS.growth_k, FLAGS.layers_per_block, FLAGS.num_classes, FLAGS.ckpt)
+        layers_per_block = [int(x) for x in FLAGS.layers_per_block.split(",")]
+        tiramisu = DenseTiramisu(FLAGS.growth_k, layers_per_block, FLAGS.num_classes)
         tiramisu.train(FLAGS.train_data, FLAGS.val_data, FLAGS.ckpt,
         FLAGS.batch_size, FLAGS.epochs, FLAGS.learning_rate, FLAGS.prior_model)
     elif FLAGS.mode == 'infer':
+        growth_k, layers_per_block, num_classes = load_model_params(FLAGS.ckpt)
+        layers_per_block = [int(x) for x in layers_per_block.split(",")]
+        tiramisu = DenseTiramisu(growth_k, layers_per_block, num_classes)
         tiramisu.infer(FLAGS.infer_data, 1, FLAGS.ckpt, FLAGS.output_folder)
 
 
