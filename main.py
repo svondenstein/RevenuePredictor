d01af87f6964ae64693fbd1effc8b2bbd6d9c8d8
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-09 17:15:50 -0500

    Update main to handle new structure.

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
 #
 # Stephen Vondenstein, Matthew Buckley
-# 10/07/2018
+# 10/08/2018
 #
-import argparse
-from helpers import get_data_paths_list
-from helpers import save_model_params
-from helpers import load_model_params
-from model import DenseTiramisu
+import tensorflow as tf
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--mode", default="infer")
-parser.add_argument("--train_data", default="train",
-                    help="Directory for training images")
-parser.add_argument("--val_data", default="train",
-                    help="Directory for validation images")
-parser.add_argument("--ckpt", default="checkpoints/model.ckpt",
-                    help="Directory for storing model checkpoints")
-parser.add_argument("--layers_per_block", default="4,5,7,10,12,15",
-                    help="Number of layers in dense blocks")
-parser.add_argument("--batch_size", default=4,
-                    help="Batch size for use in training", type=int)
-parser.add_argument("--epochs", default=12,
-                    help="Number of epochs for training", type=int)
-parser.add_argument("--num_threads", default=2,
-                    help="Number of threads to use for data input pipeline", type=int)
-parser.add_argument("--growth_k", default=16, help="Growth rate for Tiramisu", type=int)
-parser.add_argument("--num_classes",   default=2, help="Number of classes", type=int)
-parser.add_argument("--learning_rate", default=1e-4,
-                    help="Learning rate for optimizer", type=float)
-parser.add_argument("--infer_data", default="./test/")
-parser.add_argument("--output_folder", default="./predictions/")
-parser.add_argument("--prior_model", default="")
-
+from models.tiramisu import Tiramisu
+from utils.preprocess import DataGenerator
+from utils.parser import get_config
+from utils.utility import create_dirs
+from utils.logger import Logger
+from utils.rle import prepare_submission
+from helpers.trainer import Trainer
+from helpers.predicter import Predicter
 
 
 def main():
-    FLAGS = parser.parse_args()
+    # Get configuration
+    config = get_config()
 
-    if FLAGS.mode == 'train':
-        save_model_params(FLAGS.growth_k, FLAGS.layers_per_block, FLAGS.num_classes, FLAGS.ckpt)
-        layers_per_block = [int(x) for x in FLAGS.layers_per_block.split(",")]
-        tiramisu = DenseTiramisu(FLAGS.growth_k, layers_per_block, FLAGS.num_classes)
-        tiramisu.train(FLAGS.train_data, FLAGS.val_data, FLAGS.ckpt,
-        FLAGS.batch_size, FLAGS.epochs, FLAGS.learning_rate, FLAGS.prior_model)
-    elif FLAGS.mode == 'infer':
-        growth_k, layers_per_block, num_classes = load_model_params(FLAGS.ckpt)
-        layers_per_block = [int(x) for x in layers_per_block.split(",")]
-        tiramisu = DenseTiramisu(growth_k, layers_per_block, num_classes)
-        tiramisu.infer(FLAGS.infer_data, 1, FLAGS.ckpt, FLAGS.output_folder)
+    # Set up test/train environment
+    if config.predict or config.train:
+        # Set up common environment
+        sess = tf.Session()
+        data = DataGenerator(config)
+        model = Tiramisu(data, config)
+        logger = Logger(sess, config)
+
+        if config.train:
+            create_dirs([config.model_path])
+            trainer = Trainer(sess, model, data, config, logger)
+            model.load(sess)
+            trainer.train()
+
+        if config.predict:
+            predicter = Predicter(sess, model data, config, logger)
+            model.load(sess)
+            predicter.predict()
+
+    # Prepare submission
+    if config.rle:
+        prepare_submission(config.predictions, config.submissions, config.image_height, config.image_width)
+
+    print('Done!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ utils/parser.py
@@ -0,0 +1,83 @@
+#
+# Stephen Vondenstein, Matthew Buckley
+# 10/08/2018
+#
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-a', '--augment',
+                        action='store_true',
+                        default=false,
+                        help='Augment data to increase the size and variation of the training set')
+
+    parser.add_argument('-b', '--batch_size',
+                        help='Batch size for use in training',
+                        type=int)
+
+    parser.add_argument('-c', '--classes',
+                        default=2,
+                        help='Number of classes',
+                        type=int)
+
+    parser.add_argument('-d', '--data_path',
+                        default='./data/',
+                        help='Path to data directory')
+
+    parser.add_argument('-e', '--epochs',
+                        default=32,
+                        help='Number of epochs for training',
+                        type=int)
+
+    parser.add_argument('-i', '--infer',
+                        action='store_true',
+                        default=false,
+                        help='Perform inference')
+
+    parser.add_argument('-k', '--growth_k',
+                        default=16,
+                        help='Growth rate',
+                        type=int)
+
+    parser.add_argument('-lr', '--learning_rate',
+                        default=1e-3,
+                        help='Learning rate for optimizer',
+                        type=float)
+
+    parser.add_argument('-m', '--model_path',
+                        default='./models/tiramisu',
+                        help='Directory to save trained models')
+
+    parser.add_argument('-p', '--prediction_path',
+                        default='./predictions/',
+                        help='Directory to save predicted masks')
+
+    parser.add_argument('-r', '--rle',
+                        action='store_true',
+                        default=false,
+                        help='Compute RLE and prepare submission')
+
+    parser.add_argument('-s', '--submission_path',
+                        default='./submissions/',
+                        help='Directory to save submissions',
+                        type=float)
+
+    parser.add_argument('-t', '--train',
+                        action='store_true',
+                        default=false,
+                        help='Perform model training')
+
+    parser.add_argument('-v', '--validation_split',
+                        default=0.25,
+                        help='Validation split for training',
+                        type=float)
+
+    FLAGS = parser.parse_args()
+    return FLAGS
+
+
+def get_config():
+    config = get_args()
+    return config
