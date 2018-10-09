0b47a14a79e4302d942a0227550b8df27b983aff
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-09 04:27:12 -0500

    Rewrite for internal use

--- utils/rle.py
+++ utils/rle.py
@@ -2,25 +2,18 @@
 # Stephen Vondenstein
 # 10/03/2018
 #
-
-import argparse
 import pandas as pd
 import numpy as np
 import os
 from keras.preprocessing import image
 from skimage.transform import resize
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--source_folder", default="predictions")
-parser.add_argument("--image_height", default=101)
-parser.add_argument("--image_width", default=101)
-parser.add_argument("--csv", default="./submissions/submission.csv")
 
-def RLenc(img, order='F', format=True):
+def rle(img, order='F', format=True):
     bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
-    runs = []  ## list of run lengths
-    r = 0  ## the current run length
-    pos = 1  ## count starts from 1 per WK
+    runs = []  # list of run lengths
+    r = 0  # the current run length
+    pos = 1  # count starts from 1 per WK
     for c in bytes:
         if (c == 0):
             if r != 0:
@@ -46,17 +39,17 @@ def RLenc(img, order='F', format=True):
     else:
         return runs
 
-def main():
-    FLAGS = parser.parse_args()
-    pred_ids = next(os.walk(FLAGS.source_folder))[2]
-    preds = np.zeros((len(pred_ids), FLAGS.image_width, FLAGS.image_width), dtype=np.uint8)
+
+def prepare_submission(source_dir, output_path, image_height, image_width):
+    pred_ids = next(os.walk(source_dir))[2]
+    preds = np.zeros((len(pred_ids), image_width, image_width), dtype=np.uint8)
     
-    print("Resizing " + str(len(pred_ids)) + " images to " + str(FLAGS.image_width) + " by "  + str(FLAGS.image_height) + "...")
+    print("Resizing " + str(len(pred_ids)) + " images to " + str(image_width) + " by " + str(image_height) + "...")
     for i, id_ in enumerate(pred_ids):
-        path = FLAGS.source_folder
-        img = image.load_img(path + '/' +  id_)
-        x = image.img_to_array(img)[:,:,1]
-        x = resize(x, (FLAGS.image_width, FLAGS.image_height), mode='constant', preserve_range=True)
+        path = source_dir
+        img = image.load_img(path + '/' + id_)
+        x = image.img_to_array(img)[:, :, 1]
+        x = resize(x, (image_width, image_height), mode='constant', preserve_range=True)
         preds[i] = x
         if i % (len(pred_ids)/5) == 0 and i != 0:
             print("Resized " + str(i) + " images [" + str(100*i/len(pred_ids)) + "%]")
@@ -65,17 +58,15 @@ def main():
     print("Computing RLE of " + str(len(pred_ids)) + " images...")
     pred_dict = {}
     for i, fn in enumerate(pred_ids):
-        pred_dict_val = {fn[:-4]:RLenc(np.round(preds[i]))}
+        pred_dict_val = {fn[:-4]: rle(np.round(preds[i]))}
         pred_dict.update(pred_dict_val)
         if i % (len(pred_ids)/5) == 0 and i != 0:
             print("Computed RLE for " + str(i) + " images [" + str(100*i/len(pred_ids)) + "%]")
     print("Computed RLE for " + str(len(pred_ids)) + " images [100.0%]")
 
-    sub = pd.DataFrame.from_dict(pred_dict,orient='index')
+    print('Preparing submission...')
+    sub = pd.DataFrame.from_dict(pred_dict, orient='index')
     sub.index.names = ['id']
     sub.columns = ['rle_mask']
-    sub.to_csv(FLAGS.csv)
-    print("Submission saved to " + FLAGS.csv)
-
-if __name__ == "__main__":
-    main()
+    sub.to_csv(output_path)
+    print("Submission saved to " + output_path)
