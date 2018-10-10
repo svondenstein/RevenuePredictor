38f641fcb8f6613e3ce51ca44d4dc1dc74250632
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-10 04:03:56 -0500

    Fix method calls

--- helpers/preprocess.py
+++ helpers/preprocess.py
@@ -11,7 +11,6 @@ def process(dataset, training, config):
 
     return dataset
 
-@staticmethod
 def parse_data(image_paths, label_paths):
     image_content = tf.read_file(image_paths)
     images = tf.image.decode_png(image_content, channels=1)
@@ -21,14 +20,12 @@ def parse_data(image_paths, label_paths):
 
     return images, masks
 
-@staticmethod
 def resize_data(image, mask):
     image = tf.image.resize_images(image, [128, 128])
     mask = tf.image.resize_images(mask, [128, 128])
 
     return image, mask
 
-@staticmethod
 def normalize_data(image, mask):
     image = image / 255.0
     mask = mask / 255.0
