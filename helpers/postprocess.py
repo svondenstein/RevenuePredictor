76779c4546ff781931c3f381bb80e09c6fcb99b9
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-10 12:26:35 -0500

    Removed severely annoying print statement

--- helpers/postprocess.py
+++ helpers/postprocess.py
@@ -10,5 +10,4 @@ import numpy as np
 def process(image, name, config):
     for i in range(image[0].shape[0]):
         img = np.reshape(np.argmax(image[0], axis=3), (image[0].shape[0],128,128))
-        print(img.shape)
         cv2.imwrite(os.path.join(config.prediction_path, name[i].decode('utf-8')), 255 * img[i, :, :])
