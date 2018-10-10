1a8a7fc9873d1d8a10d4577078306d44299c8bd4
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-10 10:10:38 -0500

    Fix batching for inference

--- helpers/postprocess.py
+++ helpers/postprocess.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 
 def process(image, name, config):
-    for i in range(len(image)):
-        img = np.reshape(np.argmax(image[i], axis=3), (128, 128, 1))
-        cv2.imwrite(os.path.join(config.prediction_path, name[i].decode('utf-8')), 255 * img)
+    for i in range(image[0].shape[0]):
+        img = np.reshape(np.argmax(image[0], axis=3), (image[0].shape[0],128,128))
+        print(img.shape)
+        cv2.imwrite(os.path.join(config.prediction_path, name[i].decode('utf-8')), 255 * img[i, :, :])
