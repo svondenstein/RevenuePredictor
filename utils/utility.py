774a973445b5e1c2a323989201e21f390a83d115
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-10 02:42:02 -0500

    Remove unused function

--- utils/utility.py
+++ utils/utility.py
@@ -38,30 +38,3 @@ class AverageMeter:
     @property
     def val(self):
         return self.avg
-
-
-class AverageMeterList:
-    def __init__(self, num_cls):
-        self.cls = num_cls
-        self.value = [0] * self.cls
-        self.avg = [0] * self.cls
-        self.sum = [0] * self.cls
-        self.count = [0] * self.cls
-        self.reset()
-
-    def reset(self):
-        self.value = [0] * self.cls
-        self.avg = [0] * self.cls
-        self.sum = [0] * self.cls
-        self.count = [0] * self.cls
-
-    def update(self, val, n=1):
-        for i in range(self.cls):
-            self.value[i] = val[i]
-            self.sum[i] += val[i] * n
-            self.count[i] += n
-            self.avg[i] = self.sum[i] / self.count[i]
-
-    @property
-    def val(self):
-        return self.avg
