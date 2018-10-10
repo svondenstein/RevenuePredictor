08e8b1510565f482bb2537b10f1faba74e55636e
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-09 22:08:10 -0500

    Fix batch size

--- utils/parser.py
+++ utils/parser.py
@@ -14,6 +14,7 @@ def get_args():
                         help='Augment data to increase the size and variation of the training set')
 
     parser.add_argument('-b', '--batch_size',
+                        default=16,
                         help='Batch size for use in training',
                         type=int)
 
