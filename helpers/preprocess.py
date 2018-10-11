a5414871b189fa770e12d00b3a295185ec7dc5b0
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-10 21:06:46 -0500

    Data augmentation

--- helpers/preprocess.py
+++ helpers/preprocess.py
@@ -3,11 +3,14 @@
 # 10/09/2018
 #
 import tensorflow as tf
+from imgaug import augmenters as iaa
 
 def process(dataset, training, config):
     dataset = dataset.map(parse_data, num_parallel_calls=config.batch_size)
     dataset = dataset.map(resize_data, num_parallel_calls=config.batch_size)
     dataset = dataset.map(normalize_data, num_parallel_calls=config.batch_size)
+    if config.augment and training:
+        dataset = dataset.concatenate(augment_data(dataset, config))
 
     return dataset
 
@@ -34,3 +37,17 @@ def normalize_data(image, mask, name):
     mask = mask / 255.0
 
     return image, mask, name
+
+def augment_data(dataset, config):
+    dataset = dataset.map(flip_data, num_parallel_calls=config.batch_size)
+
+    return dataset
+
+def flip_data(image, mask, name):
+    seq = iaa.Sequential({
+        iaa.Fliplr(0.5)
+    }, random_order=True)
+    image_aug = seq.augment_image(image)
+    mask_aug = seq.augment_image(mask)
+
+    return image_aug, mask_aug, name
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ionic',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/svondenstein/ionic',
+    license='MIT',
+    author='Matthew Buckley, Stephen Vondenstein',
+    author_email='',
+    description='Identifies Salt Deposits by analyzing subsurface images.'
+)
