5079e9c6ea54f3930cf17411fcbad2e206a5687c
Matt Buckley <mattadored@gmail.com>
2018-10-13 04:14:34 +0000

    implemented preprocess input tiling

--- helpers/preprocess.py
+++ helpers/preprocess.py
@@ -4,6 +4,7 @@
 #
 import tensorflow as tf
 
+
 def process(dataset, training, config, len):
     dataset = dataset.map(parse_data, num_parallel_calls=config.batch_size)
     dataset = dataset.map(resize_data, num_parallel_calls=config.batch_size)
@@ -15,6 +16,7 @@ def process(dataset, training, config, len):
 
     return dataset
 
+
 def parse_data(image_paths, label_paths, names):
     image_content = tf.read_file(image_paths)
     images = tf.image.decode_png(image_content, channels=1)
@@ -24,11 +26,13 @@ def parse_data(image_paths, label_paths, names):
 
     return images, masks, names
 
+
 def resize_data(image, mask, name):
     image = tf.image.resize_images(image, [128, 128])
     mask = tf.image.resize_images(mask, [128, 128])
 
     return image, mask, name
+
 
 def normalize_data(image, mask, name):
     image = tf.cast(image, tf.float32)
@@ -39,13 +43,30 @@ def normalize_data(image, mask, name):
 
     return image, mask, name
 
+
 def augment_data(dataset, config):
     dataset = dataset.map(flip_data, num_parallel_calls=config.batch_size)
 
     return dataset
 
+
+# Tiles the image with flipped tiles and then crops
+def tile_image(image):
+    top_flip = tf.image.flip_up_down(image)
+    tall_image = tf.concat([top_flip,image,top_flip], 1)
+    tall_flipped = tf.image.flip_left_right(tall_image)
+    complete_tiled = tf.concat([tall_flipped,tall_image,tall_flipped], 2)
+    tf.image.crop_to_bounding_box(complete_tiled,(101-13),(101-13),128,128)
+    return image
+
+
+def tile_data(image, mask, name):
+    image = tile_image(image)
+    mask = tile_image(mask)
+    return image, mask, name
+
+
 def flip_data(image, mask, name):
     image = tf.image.flip_left_right(image)
     mask = tf.image.flip_left_right(mask)
-
     return image, mask, name
