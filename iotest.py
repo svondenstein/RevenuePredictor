f660079165664282cf46f8423a308d8a9ca9b9b4
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-14 20:35:18 -0500

    Added print statements for legibility

--- iotest.py
+++ iotest.py
@@ -7,6 +7,7 @@ import cv2
 import os, subprocess
 import tensorflow as tf
 
+from tqdm import tqdm
 from utils.parser import get_args
 from utils.utility import create_dirs
 from utils.rle import prepare_submission
@@ -14,34 +15,57 @@ from helpers.postprocess import data_crop
 from helpers.preprocess import tile_data
 
 def main():
+    # Set Tensorflow verbosity
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
     config = get_args()
+    print('Creating save directories...')
     create_dirs(['./image_tests/',
                  './image_tests/tiled/', './image_tests/tiled/images/', './images_tests/tiled/masks/',
                  './image_tests/detiled', './image_tests/detiled/images/', './images_tests/detiled/masks/',
                  config.submission_path])
+    print('Initializing data loader...')
     data_loader = DataGenerator(config)
+
+    # Progress bar
+    batches = tqdm(range(data_loader.num_iterations_train), total=data_loader.num_iterations_train,
+              desc="Processing Batches ")
+
+    # Use session to evaluate real tensors
     with tf.Session() as sess:
+        # Initialize data generator
         data_loader.initialize(sess, 'train')
-        for i in range(data_loader.num_iterations_train):
+        for i in range(batches):
+            # Get a batch and tile it
             image, mask, name = sess.run(tile_data(*data_loader.get_input()))
+            # Save tiled images
             for i in range(config.batch_size):
                 cv2.imwrite(os.path.join('./image_tests/tiled/images', name[i].decode('utf-8')), 255 * image[i, :, :])
                 cv2.imwrite(os.path.join('./image_tests/tiled/masks', name[i].decode('utf-8')), 255 * mask[i, :, :])
+            # Convert images back to tensors because that's how the de-tiler expects them
             image = tf.convert_to_tensor(image)
             mask = tf.convert_to_tensor(mask)
             name = tf.convert_to_tensor(name)
+            # De-tile images
             image, mask, name = sess.run(data_crop(image, mask, name))
+            # Save de-tiled images
             for i in range(config.batch_size):
                 cv2.imwrite(os.path.join('./image_tests/detiled/images', name[i].decode('utf-8')), 255 * image[i, :, :])
                 cv2.imwrite(os.path.join('./image_tests/detiled/masks', name[i].decode('utf-8')), 255 * mask[i, :, :])
 
-        rle = prepare_submission('./image_tests/detiled/', config.submission_path, 'iotest')
-        subprocess.Popen("sort ./data/train.csv > ./image_tests/source.csv", shell=True)
-        subprocess.Popen("sort " + rle + " > ./image_tests/test.csv", shell=True)
-        print(subprocess.Popen("diff -as ./data/test.csv ./data/test2.csv", shell=True,
-                               stdout=subprocess.PIPE).stdout.readline().decode(), end='')
+    # Close progress bar
+    batches.close()
+    # Compute RLEs for saved masks
+    rle = prepare_submission('./image_tests/detiled/', config.submission_path, 'iotest')
+    print('Comparing RLE values...')
+    subprocess.Popen("sort ./data/train.csv > ./image_tests/source.csv", shell=True)
+    subprocess.Popen("sort " + rle + " > ./image_tests/test.csv", shell=True)
+    # Compare RLEs for saved masks
+    print(subprocess.Popen("diff -as ./data/test.csv ./data/test2.csv", shell=True,
+                           stdout=subprocess.PIPE).stdout.readline().decode(), end='')
 
-        print('Done!')
+    print('Done!')
+
 
 if __name__ == '__main__':
     main()
