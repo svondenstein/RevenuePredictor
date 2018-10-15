40179973cb40f4ce9b05d3b2dca0311a45b1654b
Stephen Vondenstein <stephen@vondenstein.com>
2018-10-15 05:52:12 -0500

    IoU is finally correct.

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,24 +3,32 @@
 # 10/09/2018
 #
 import tensorflow as tf
+import numpy as np
 
 
-# TODO: Fix this, I think it's incorrect.
-def iou(prediction, mask, num_classes):
-    print('Mask shape: ' + str(mask.get_shape()))
-    print('Raw prediction shape: ' + str(prediction.get_shape()))
-    prediction = tf.cast(tf.expand_dims(tf.argmax(prediction, axis=3), [-1]), tf.float32)
-    print('Argmax prediction shape: ' + str(prediction.get_shape()))
-    inter = tf.reduce_sum(tf.multiply(prediction, mask))
-    print('Intersection shape: ' + str(inter.get_shape()))
-    union = tf.reduce_sum(tf.add(prediction, mask))
-    print('Union shape: ' + str(union.get_shape()))
-    raw_iou = tf.divide(inter, tf.subtract(union, inter))
-    print('Raw IoU shape: ' + str(raw_iou.get_shape()))
-    mean_iou = tf.reduce_mean(raw_iou)
-    print('Mean IoU shape: ' + str(mean_iou.get_shape()))
+def iou(prediction, mask, batch_size):
+    metric = []
+    epsilon = 1e-15
+    predictions = tf.unstack(prediction, num=batch_size)
+    masks = tf.unstack(mask, num=batch_size)
+    for i in range(batch_size):
+        s = masks[i]
+        t = predictions[i]
+        if tf.reduce_sum(s) == 0 and tf.reduce_sum(t) == 0:
+            metric.append(1.0)
+        elif tf.reduce_sum(s) == 0 and tf.reduce_sum(t) != 0:
+            metric.append(0.0)
+        else:
+            intersection = tf.reduce_sum(tf.multiply(s, t))
+            union = tf.subtract(tf.add(tf.reduce_sum(s), tf.reduce_sum(t)), intersection)
+            iou = tf.divide(tf.add(intersection, epsilon), tf.add(union, epsilon))
+            thresholds = np.arange(0.5, 0.95, 0.05)
+            miou = []
+            for thresh in thresholds:
+                miou.append(tf.cond(iou > thresh, true_fn=lambda: 1, false_fn=lambda: 0))
+            metric.append(tf.reduce_mean(miou))
 
-    return mean_iou
+    return tf.reduce_mean(metric)
 
 
 def cross_entropy(prediction, mask, num_classes):
