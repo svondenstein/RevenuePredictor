80ad025d6b5f9afcc03cdb78985e9e38f18153ae
Stephen Vondenstein <shvondenstein@gmail.com>
2018-10-17 22:47:02 -0500

    IoU accurate to Kaggle specification. Cross entropy now uses offical tf.keras implementation.

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -6,34 +6,23 @@ import tensorflow as tf
 import numpy as np
 
 def iou(prediction, mask, name):
-    metric = []
-    elems = (prediction, mask)
-    metric.append(tf.reduce_mean(tf.map_fn(mean_iou, elems, dtype=tf.int32)))
-
-    return tf.reduce_mean(metric, name=name)
-
-def mean_iou(elems):
-    epsilon = 1e-15
-    s = tf.cast(elems[1], tf.float32)
-    t = elems[0]
-    if tf.reduce_sum(s) == 0 and tf.reduce_sum(t) == 0:
-        return 1.0
-    elif tf.reduce_sum(s) == 0 and tf.reduce_sum(t) != 0:
-        return 0.0
-    else:
-        intersection = tf.reduce_sum(tf.multiply(s, t))
-        union = tf.subtract(tf.add(tf.reduce_sum(s), tf.reduce_sum(t)), intersection)
-        iou = tf.divide(tf.add(intersection, epsilon), tf.add(union, epsilon))
-        thresholds = np.arange(0.5, 0.95, 0.05)
-        miou = []
-        for thresh in thresholds:
-            miou.append(tf.cond(iou > thresh, true_fn=lambda: 1, false_fn=lambda: 0))
-        return tf.reduce_mean(miou)
-
+    # Compute the argmax for the output to match mask shape
+    prediction = tf.expand_dims(tf.argmax(prediction, axis=-1), axis=-1)
+    # Thresholds as specified by competition
+    thresholds = np.arange(0.5, 0.95, 0.05, dtype='float32')
+    # Compute precion at thresholds
+    precision = tf.metrics.precision_at_thresholds(mask, prediction, thresholds)
+    # Compute the mean for this step
+    mean_iou = tf.reduce_mean(precision, name=name)
+    
+    return mean_iou
 
 def cross_entropy(prediction, mask, num_classes, name):
+    # Encode mask as one_hot to match prediction
     mask = tf.one_hot(tf.squeeze(mask), num_classes, dtype=tf.float32)
-    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=mask, logits=prediction)
+    # Compute binary cross_entropy
+    loss = tf.keras.backend.binary_crossentropy(mask, prediction)
+    # Compute the mean for this step
     loss = tf.reduce_mean(loss, name=name)
 
     return loss
